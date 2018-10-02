fb0d5b50c5e23aa5ca7c3c226a499da65b7a6d92
AlexandrePetit-Pas <petitpasalexandre@gmail.com>
2018-10-02 10:51:20 +0200

    test: add getProfile test

--- test/AuthService.test.ts
+++ test/AuthService.test.ts
@@ -1,5 +1,6 @@
 import AuthService from "../src/AuthService";
 import { LocalStorageMock } from "./LocalStorageMock";
+import * as jwtDecode from "jwt-decode";
 
 test("testing loggedIn", () => {
   var auth = new AuthService();
@@ -26,6 +27,22 @@ test("testing getToken toto", () => {
   (global as any).localStorage  = new LocalStorageMock(jest);
 
   expect(auth.getToken()).toBeNull();
+
   auth.setToken("toto");
   expect(auth.getToken()).toEqual("toto");
+
+  auth.logout();
+  expect(auth.getToken()).toBeNull();
+});
+
+test("testing getProfile", () => {
+  var auth = new AuthService();
+  (global as any).localStorage  = new LocalStorageMock(jest);
+
+  let token: String = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWI"
+  + "iOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE"
+  + "2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
+  auth.setToken(token);
+  expect(auth.getProfile()).toEqual(jwtDecode(token));
 });
