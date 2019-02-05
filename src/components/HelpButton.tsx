91330a93f40ea781e40574e2bdbb9040c57cd4c1
Friigy <bastian.szczygielski@gmail.com>
2019-02-05 22:50:54 +0100

    bonsoir

--- src/components/HelpButton.tsx
+++ src/components/HelpButton.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Rail, Sticky, Icon, Accordion, Segment, Container } from "semantic-ui-react";
+import { Rail, Sticky, Icon, Accordion, Segment } from "semantic-ui-react";
 
 export default class HelpButton extends React.Component<any, any> {
     constructor(props: any) {
@@ -24,17 +24,15 @@ export default class HelpButton extends React.Component<any, any> {
         return (
             <Rail position="right">
                 <Sticky offset={70}>
-                    <Container textAlign="right">
-                        <Icon
-                            inverted={!this.state.menuOpen}
-                            circular={true}
-                            name="question"
-                            color="orange"
-                            size="big"
-                            onClick={this.interactHelpMenu}
-                            style={{ cursor: "pointer" }}
-                        />
-                    </Container>
+                    <Icon
+                        inverted={!this.state.menuOpen}
+                        circular={true}
+                        name="question"
+                        color="orange"
+                        size="big"
+                        onClick={this.interactHelpMenu}
+                        style={{ cursor: "pointer" }}
+                    />
                 {
                     this.state.menuOpen ?
                         <Segment color="orange" inverted={true}>
@@ -49,7 +47,7 @@ export default class HelpButton extends React.Component<any, any> {
                                 </Accordion.Title>
 
                                 <Accordion.Content active={this.state.activeIndex === 0}>
-                                    Right click the file or the folder and click download.
+                                    When you are in you IDE, right click the file or the folder and click download.
                                 </Accordion.Content>
 
                                 <Accordion.Content active={this.state.activeIndex === 0}>
@@ -67,8 +65,52 @@ export default class HelpButton extends React.Component<any, any> {
                                 </Accordion.Title>
 
                                 <Accordion.Content active={this.state.activeIndex === 1}>
-                                    As you create your environment, you can choose
-                                    who to share your environment with.
+                                    Below your environment, you can manage users who have access to it.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 1}>
+                                    You can give them access for a limited time with different rights to it
+                                    (read only...).
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 2}
+                                    index={2}
+                                    onClick={this.handleClickAcc}
+                                >
+                                    <Icon name="dropdown" />
+                                    How do I create an image?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    If you want to create an image of your environment to make environments 
+                                    based on yours, click on "Save Image".
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    If your image is an update of the one being currently used, 
+                                    leave it under the same name and increment the version.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    You can choose to make this image public or private.
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 3}
+                                    index={3}
+                                    onClick={this.handleClickAcc}
+                                >
+                                    <Icon name="dropdown" />
+                                    So, what exactly is the difference between an image and an environment?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 3}>
+                                    Think of the environment as your computer, with files and folders.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 3}>
+                                    The image is the OS and contains the installed softwares.
                                 </Accordion.Content>
                             </Accordion>
                         </Segment>
