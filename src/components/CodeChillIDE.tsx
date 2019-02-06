ed31f213fe2924b05350bcff176d98701704f54f
Lulu300 <ludwigsimon77@gmail.com>
2019-02-06 10:49:36 +0100

    Fix access to IDE

--- src/components/CodeChillIDE.tsx
+++ src/components/CodeChillIDE.tsx
@@ -16,14 +16,18 @@ class CodeChillIDE extends React.Component<any, any> {
         this.docker = this.props.user.dockers.find(
             (docker: any) => docker.id === Number(this.props.props.match.params.id)
         );
+        this.state = { "src": "" };
+    }
+
+    componentDidMount() {
         if (this.docker) {
             this.src = `${(window as any).env.docker}:${this.docker["port"]}`;
-            this.state = { "src": this.src };
+            this.setState({"src": this.src});
         } else {
             this.Auth.checkUserHaveAccess(Number(this.props.props.match.params.id), this.props.user.id).then((res) => {
                 if (res.status === 200) {
                     this.src = `${(window as any).env.docker}:${res.content.docker["port"]}`;
-                    this.state = { "src": this.src };
+                    this.setState({"src": this.src});
                 } else {
                     this.props.history.replace(formatRoute(HOME));
                 }
