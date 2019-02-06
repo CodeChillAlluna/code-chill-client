f1c50f439b79f61a529fa416891ad92eb4309a06
AlexandrePetit-Pas <petitpasalexandre@gmail.com>
2019-02-06 10:05:13 +0100

    fix setState

--- src/components/CodeChillIDE.tsx
+++ src/components/CodeChillIDE.tsx
@@ -12,22 +12,18 @@ class CodeChillIDE extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
 
-        this.state = {
-            "src": ""
-        };
-
         this.Auth = new AuthService();
         this.docker = this.props.user.dockers.find(
             (docker: any) => docker.id === Number(this.props.props.match.params.id)
         );
         if (this.docker) {
             this.src = `${(window as any).env.docker}:${this.docker["port"]}`;
-            this.setState({ "src": this.src });
+            this.state = { "src": this.src };
         } else {
             this.Auth.checkUserHaveAccess(Number(this.props.props.match.params.id), this.props.user.id).then((res) => {
                 if (res.status === 200) {
                     this.src = `${(window as any).env.docker}:${res.content.docker["port"]}`;
-                    this.setState({ src: this.src });
+                    this.state = { "src": this.src };
                 } else {
                     this.props.history.replace(formatRoute(HOME));
                 }
