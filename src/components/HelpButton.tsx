1982b7469887e9cca1589853c35e2315a4bfca95
Friigy <bastian.szczygielski@gmail.com>
2019-02-16 18:49:42 +0100

    it works kinda

--- src/components/HelpButton.tsx
+++ src/components/HelpButton.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Rail, Sticky, Icon, Accordion, Segment } from "semantic-ui-react";
+import { Icon, Accordion, Segment, Container } from "semantic-ui-react";
 
 export default class HelpButton extends React.Component<any, any> {
     constructor(props: any) {
@@ -22,8 +22,121 @@ export default class HelpButton extends React.Component<any, any> {
 
     render () {
         return (
-            <Rail position="right" size="mini">
-                <Sticky offset={70}>
+            <div
+                style={{
+                    right: 8,
+                    bottom: 8,
+                    position: "fixed"
+                }}
+            >
+                {
+                    this.state.menuOpen ?
+                        <Segment color="orange" inverted={true} raised={true} compact={true} size="small">
+                            <Accordion inverted={true}>
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 0}
+                                    index={0}
+                                    onClick={this.handleClickAcc}
+                                >
+                                <Icon name="dropdown" />
+                                    Download/Uploading files in my environment
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 0}>
+                                    When you are in you IDE, right click <br/>
+                                    the file or the folder and click download.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 0}>
+                                    You can simply drag any file or folder <br/>
+                                    from your file explorer down into your <br/>
+                                    environment.
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 1}
+                                    index={1}
+                                    onClick={this.handleClickAcc}
+                                >
+                                <Icon name="dropdown" />
+                                    How do I share my environment with others?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 1}>
+                                    Below your environment, you can manage <br/>
+                                    users who have access to it.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 1}>
+                                    You can give them access for a limited <br/>
+                                    time with different rights to it (read <br/>
+                                    only...).
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 2}
+                                    index={2}
+                                    onClick={this.handleClickAcc}
+                                >
+                                <Icon name="dropdown" />
+                                    How do I create an image?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    If you want to create an image of your <br/>
+                                    environment to make environments based <br/>
+                                    on yours, click on "Save Image".
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    If your image is an update of the one <br/>
+                                    being currently used, leave it under the <br/>
+                                    same name and increment the version.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 2}>
+                                    You can choose to make this image public <br/>
+                                    or private.
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 3}
+                                    index={3}
+                                    onClick={this.handleClickAcc}
+                                >
+                                <Icon name="dropdown" />
+                                    So, what exactly is the difference between <br/>
+                                    an image and an environment?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 3}>
+                                    Think of the environment as your computer, <br/>
+                                    with files and folders.
+                                </Accordion.Content>
+
+                                <Accordion.Content active={this.state.activeIndex === 3}>
+                                    The image is the OS and contains the installed <br/>
+                                    softwares.
+                                </Accordion.Content>
+
+                                <Accordion.Title
+                                    active={this.state.activeIndex === 4}
+                                    index={4}
+                                    onClick={this.handleClickAcc}
+                                >
+                                <Icon name="dropdown" />
+                                    My IDE is not showing, what do I do?
+                                </Accordion.Title>
+
+                                <Accordion.Content active={this.state.activeIndex === 4}>
+                                    If your IDE is not showing, refresh the page <br/>
+                                    and wait a little.
+                                </Accordion.Content>
+                            </Accordion>
+                        </Segment>
+                    : null
+                }
+                <Container fluid={true} textAlign="right">
                     <Icon
                         inverted={!this.state.menuOpen}
                         circular={true}
@@ -33,91 +146,8 @@ export default class HelpButton extends React.Component<any, any> {
                         onClick={this.interactHelpMenu}
                         style={{ cursor: "pointer" }}
                     />
-                {
-                    this.state.menuOpen ?
-                        <Segment color="orange" inverted={true} size="mini">
-                            <Accordion inverted={true}>
-                                <Accordion.Title
-                                    active={this.state.activeIndex === 0}
-                                    index={0}
-                                    onClick={this.handleClickAcc}
-                                >
-                                    <Icon name="dropdown" />
-                                    Download/Uploading files in my environment
-                                </Accordion.Title>
-
-                                <Accordion.Content active={this.state.activeIndex === 0}>
-                                    When you are in you IDE, right click the file or the folder and click download.
-                                </Accordion.Content>
-
-                                <Accordion.Content active={this.state.activeIndex === 0}>
-                                    You can simply drag any file or folder from
-                                    your file explorer down into your environment.
-                                </Accordion.Content>
-
-                                <Accordion.Title
-                                    active={this.state.activeIndex === 1}
-                                    index={1}
-                                    onClick={this.handleClickAcc}
-                                >
-                                    <Icon name="dropdown" />
-                                    How do I share my environment with others?
-                                </Accordion.Title>
-
-                                <Accordion.Content active={this.state.activeIndex === 1}>
-                                    Below your environment, you can manage users who have access to it.
-                                </Accordion.Content>
-
-                                <Accordion.Content active={this.state.activeIndex === 1}>
-                                    You can give them access for a limited time with different rights to it
-                                    (read only...).
-                                </Accordion.Content>
-
-                                <Accordion.Title
-                                    active={this.state.activeIndex === 2}
-                                    index={2}
-                                    onClick={this.handleClickAcc}
-                                >
-                                    <Icon name="dropdown" />
-                                    How do I create an image?
-                                </Accordion.Title>
-
-                                <Accordion.Content active={this.state.activeIndex === 2}>
-                                    If you want to create an image of your environment to make environments 
-                                    based on yours, click on "Save Image".
-                                </Accordion.Content>
-
-                                <Accordion.Content active={this.state.activeIndex === 2}>
-                                    If your image is an update of the one being currently used, 
-                                    leave it under the same name and increment the version.
-                                </Accordion.Content>
-
-                                <Accordion.Content active={this.state.activeIndex === 2}>
-                                    You can choose to make this image public or private.
-                                </Accordion.Content>
-
-                                <Accordion.Title
-                                    active={this.state.activeIndex === 3}
-                                    index={3}
-                                    onClick={this.handleClickAcc}
-                                >
-                                    <Icon name="dropdown" />
-                                    So, what exactly is the difference between an image and an environment?
-                                </Accordion.Title>
-
-                                <Accordion.Content active={this.state.activeIndex === 3}>
-                                    Think of the environment as your computer, with files and folders.
-                                </Accordion.Content>
-
-                                <Accordion.Content active={this.state.activeIndex === 3}>
-                                    The image is the OS and contains the installed softwares.
-                                </Accordion.Content>
-                            </Accordion>
-                        </Segment>
-                    : null
-                }
-                </Sticky>
-            </Rail>
+                </Container>
+            </div>
         );
     }
 }
